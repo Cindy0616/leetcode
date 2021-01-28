b14b81a4e60faddd47e92d8b0ac7e1e25aa81d66
Cindy Liu <chengjing0616@gmail.com>
2021-01-27 22:32:37 -0800

    implementing linked list add two numbers

--- linkedList/addTwoNumber.js
+++ linkedList/addTwoNumber.js
@@ -54,5 +54,19 @@ var addTwoNumbers = function(l1, l2){
       sum += l1.val;
       l1 = l1.next;
     }
+    if( l2 ){
+      sum += l2.val;
+      l2 = l2.next;
+    }
+    if(sum > 9) {
+      sum = sum %10;
+      carry = 1;
+    }
+    head.next = new ListNode(sum);
+    head = head.next;
+
+    sum = carry;
+    carry = 0;
   }
+  return List.next;
 }
